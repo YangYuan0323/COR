52037aa29ebd5e8b74e8e884e3b84dd541a3f6dc
YangYuan0323 <1742427104@qq.com>
2018-12-02 09:05:14 +0800

    first commit

---
+++ src/com/imooc/pattern/cor/Customer.java
@@ -0,0 +1,37 @@
+package com.imooc.pattern.cor;
+
+import java.util.Random;
+import com.imooc.pattern.cor.handler.PriceHandler;
+import com.imooc.pattern.cor.handler.PriceHandlerFactory;
+
+/*
+ * 责任链模式
+ * 客户，请求折扣
+ */
+public class Customer {
+	
+	private PriceHandler priceHandler;
+	
+	public void setPriceHandler(PriceHandler priceHandler) {
+		this.priceHandler = priceHandler;
+	}
+
+	public void requestDiscount(float discount){
+		priceHandler.processDiscount(discount);
+	}
+	
+	
+	public static void main(String[] args){
+		Customer customer = new Customer();
+		customer.setPriceHandler(PriceHandlerFactory.createPriceHandler());
+		
+		Random rand = new Random();
+		
+		for(int i=1;i<=100;i++){
+			System.out.print(i+":");
+			customer.requestDiscount(rand.nextFloat());
+		}
+	}
+	
+
+}
